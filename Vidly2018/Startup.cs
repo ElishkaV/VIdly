dbae89613a3ed6a560ec995fba04c7b1c7db6677
Vasserman2000 <vasserman2000@gmail.com>
2018-01-21 21:52:08 +0200

    initial

---
+++ Vidly2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly2018.Startup))]
+namespace Vidly2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
