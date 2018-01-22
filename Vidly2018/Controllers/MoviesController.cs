1264ea13c4e31121bacaa4588dac97e73428d3e4
Vasserman2000 <vasserman2000@gmail.com>
2018-01-22 21:39:31 +0200

    Section 1 finished

---
+++ Vidly2018/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2018.Models;
+
+namespace Vidly2018.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek!"};
+            return View(movie);
+        }
+    }
+}
