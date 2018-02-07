81d209b1ba04d6507762f630326d860a0a92bbf1
Vasserman2000 <vasserman2000@gmail.com>
2018-02-07 22:13:10 +0200

    Web API exercise still in progress

---
+++ Vidly2018/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly2018.DTOs;
+using Vidly2018.Models;
+
+namespace Vidly2018.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, MovieDto>();
+            CreateMap<MovieDto, Movie>();
+        }
+    }
+}
